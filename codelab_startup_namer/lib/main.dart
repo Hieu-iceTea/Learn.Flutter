4cecfa3c6daac315790cb21808ce403474915b12
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-19 11:09:37 +0700

    NEW: codelab_startup_namer -5. Add a stateful widget

--- codelab_startup_namer/lib/main.dart
+++ codelab_startup_namer/lib/main.dart
@@ -10,18 +10,36 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random(); // Add this line.
+    //final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(                       // Drop the const, and
+        body: const Center(                       // Drop the const, and
           //child: Text('Hello World'),     // Replace this text...
-          child: Text(wordPair.asPascalCase),  // With this text.
+          //child: Text(wordPair.asPascalCase),  // With this text.
+
+          //child: Text(wordPair.asPascalCase), // REPLACE with...
+          child: RandomWords(),                 // ...this line
         ),
       ),
     );
+  }
+}
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    //return Container();
+    final wordPair = WordPair.random();      // NEW
+    return Text(wordPair.asPascalCase);      // NEW
   }
 }
