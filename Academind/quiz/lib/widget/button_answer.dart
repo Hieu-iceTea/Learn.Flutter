e9fd335a1bc6cd4afeca8737009508be9a3dae88
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-21 15:00:25 +0700

    UPDATE: style: ElevatedButton

--- Academind/quiz/lib/widget/button_answer.dart
+++ Academind/quiz/lib/widget/button_answer.dart
@@ -5,12 +5,18 @@ class ButtonAnswer extends StatelessWidget {
   final Answer answer;
   final Function onPressed;
 
-  const ButtonAnswer({required this.answer, required this.onPressed, Key? key}) : super(key: key);
+  const ButtonAnswer({required this.answer, required this.onPressed, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ElevatedButton(onPressed: () => onPressed(answer.score), child: Text(answer.title)),
+      //width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () => onPressed(answer.score),
+        child: Text(answer.title),
+        style: ElevatedButton.styleFrom(fixedSize: const Size.fromWidth(250)),
+      ),
     );
   }
 }
