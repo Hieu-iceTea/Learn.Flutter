fef96a1348d616ceb9788799cce39249b3e25560
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-22 23:14:08 +0700

    UPDATE: personal_expenses_origin

--- Academind/personal_expenses_origin/lib/main.dart
+++ Academind/personal_expenses_origin/lib/main.dart
@@ -169,12 +169,8 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
   }
 
-  @override
-  Widget build(BuildContext context) {
-    print('build() MyHomePageState');
-    final mediaQuery = MediaQuery.of(context);
-    final isLandscape = mediaQuery.orientation == Orientation.landscape;
-    final PreferredSizeWidget appBar = Platform.isIOS
+  Widget _buildAppBar() {
+    return Platform.isIOS
         ? CupertinoNavigationBar(
             middle: Text(
               'Personal Expenses',
@@ -199,7 +195,15 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () => _startAddNewTransaction(context),
               ),
             ],
-          ) as PreferredSizeWidget;
+          );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build() MyHomePageState');
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+    final PreferredSizeWidget appBar = _buildAppBar() as PreferredSizeWidget;
     final txListWidget = Container(
       height: (mediaQuery.size.height -
               appBar.preferredSize.height -
