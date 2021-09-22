069665c3c199d2cc01f2e58ed4e8210b09f14381
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-22 19:18:29 +0700

    UPDATE: personal_expenses_origin

--- Academind/personal_expenses_origin/lib/widgets/new_transaction.dart
+++ Academind/personal_expenses_origin/lib/widgets/new_transaction.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import '../widgets/adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -94,16 +97,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate!)}',
                       ),
                     ),
-                    FlatButton(
-                      textColor: Theme.of(context).primaryColor,
-                      child: Text(
-                        'Choose Date',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      onPressed: _presentDatePicker,
-                    ),
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker)
                   ],
                 ),
               ),
