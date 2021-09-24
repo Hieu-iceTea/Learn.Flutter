152edea811ccf5718e7834e744989bdc7410a76d
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-24 18:14:31 +0700

    INITIAL: deli_meals_origin

--- Academind/deli_meals_origin/lib/main.dart
+++ Academind/deli_meals_origin/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
+import './screens/filters_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/tabs_screen.dart';
 
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       onGenerateRoute: (settings) {
         print(settings.arguments);
