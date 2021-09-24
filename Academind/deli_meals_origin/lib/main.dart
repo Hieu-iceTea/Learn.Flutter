cc647eafe589c2bcad7e7792ada2093f91dc2560
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-24 13:13:36 +0700

    INITIAL: deli_meals_origin

--- Academind/deli_meals_origin/lib/main.dart
+++ Academind/deli_meals_origin/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './categories_screen.dart';
+import './category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,7 +28,12 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             )),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
