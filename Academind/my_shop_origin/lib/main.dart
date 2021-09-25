e3bc95c481d23a9830c88cd79d06f0be66ae7980
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-25 16:35:31 +0700

    INITIAL: my_shop_origin

--- Academind/my_shop_origin/lib/main.dart
+++ Academind/my_shop_origin/lib/main.dart
@@ -1,30 +1,23 @@
 import 'package:flutter/material.dart';
 
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop!'),
-      ),
-    );
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        });
   }
 }
