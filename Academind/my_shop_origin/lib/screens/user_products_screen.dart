3f1a587fb66e4226e8dabf769495f66a23cb7410
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-28 23:19:20 +0700

    update: my_shop_origin

--- Academind/my_shop_origin/lib/screens/user_products_screen.dart
+++ Academind/my_shop_origin/lib/screens/user_products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './edit_product_screen.dart';
 import '../providers/products.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
@@ -18,7 +19,7 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              // ...
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           ),
         ],
