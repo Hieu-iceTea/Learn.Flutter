9bff5182b87968c0c57db8e421b7184fbaed85c8
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-28 16:58:25 +0700

    update: my_shop_origin

--- Academind/my_shop_origin/lib/widgets/products_grid.dart
+++ Academind/my_shop_origin/lib/widgets/products_grid.dart
@@ -12,10 +12,14 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        //builder: (c) => products[i],
+        create: (c) => products[i],
+        child: ProductItem(
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
