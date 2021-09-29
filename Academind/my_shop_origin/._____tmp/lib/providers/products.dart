099920e6782463f82c0d1692725afc0b5e91f607
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-29 12:14:18 +0700

    update: my_shop_origin

--- Academind/my_shop_origin/._____tmp/lib/providers/products.dart
+++ Academind/my_shop_origin/._____tmp/lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http; 
 
 import './product.dart';
 
@@ -65,6 +68,14 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    const url = 'https://flutter-update.firebaseio.com/products.json';
+    http.post(url, body: json.encode({
+      'title': product.title,
+      'description': product.description,
+      'imageUrl': product.imageUrl,
+      'price': product.price,
+      'isFavorite': product.isFavorite,
+    }),);
     final newProduct = Product(
       title: product.title,
       description: product.description,
--- Academind/my_shop_origin/lib/providers/products.dart
+++ Academind/my_shop_origin/lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -66,6 +69,18 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    //const url = 'https://flutter-update.firebaseio.com/products.json';
+    var url = Uri.https('hieu-icetea-learn-flutter-shop-default-rtdb.asia-southeast1.firebasedatabase.app', '/products.json');
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    );
     final newProduct = Product(
       title: product.title,
       description: product.description,
