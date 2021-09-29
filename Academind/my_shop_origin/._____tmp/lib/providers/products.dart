8a337dc88dfd6234a672f2520fbb5406121014cf
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-29 15:41:25 +0700

    update: my_shop_origin

--- Academind/my_shop_origin/._____tmp/lib/providers/products.dart
+++ Academind/my_shop_origin/._____tmp/lib/providers/products.dart
@@ -91,6 +91,9 @@ class Products with ChangeNotifier {
       _items.add(newProduct);
       // _items.insert(0, newProduct); // at the start of the list
       notifyListeners();
+    }).catchError((error) {
+      print(error);
+      throw error;
     });
   }
 
--- Academind/my_shop_origin/._____tmp/lib/screens/edit_product_screen.dart
+++ Academind/my_shop_origin/._____tmp/lib/screens/edit_product_screen.dart
@@ -102,7 +102,20 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: Text('An error occurred!'),
+                content: Text('Something went wrong.'),
+                actions: <Widget>[
+                  FlatButton(child: Text('Okay'), onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },)
+                ],
+              ),
+        );
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
