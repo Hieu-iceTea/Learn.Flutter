1b7ba006465ffb5beb3f93e6659c97b34e50491c
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-30 00:10:02 +0700

    update: my_shop_origin

--- Academind/my_shop_origin/._____tmp/lib/main.dart
+++ Academind/my_shop_origin/._____tmp/lib/main.dart
@@ -10,6 +10,7 @@ import './providers/orders.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
           ),
-          home: ProductsOverviewScreen(),
+          home: AuthScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
--- Academind/my_shop_origin/lib/main.dart
+++ Academind/my_shop_origin/lib/main.dart
@@ -4,11 +4,11 @@ import 'package:provider/provider.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 import './providers/products.dart';
+import './screens/auth_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/product_detail_screen.dart';
-import './screens/products_overview_screen.dart';
 import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
           ),
-          home: ProductsOverviewScreen(),
+          home: AuthScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
