ad8f5b45be6791a0153627df29857b51c8cdfcad
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-01 09:52:04 +0700

    update: my_shop_origin

--- Academind/my_shop_origin/._____tmp/lib/widgets/app_drawer.dart
+++ Academind/my_shop_origin/._____tmp/lib/widgets/app_drawer.dart
@@ -47,7 +47,6 @@ class AppDrawer extends StatelessWidget {
             title: Text('Logout'),
             onTap: () {
               Navigator.of(context).pop();
-              Navigator.of(context).pushReplacementNamed('/');
               
               // Navigator.of(context)
               //     .pushReplacementNamed(UserProductsScreen.routeName);
