0f61e1056bf358810a9d23129f1410af9e9e3851
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-01 15:58:03 +0700

    UPDATE: great_places_origin

--- Academind/great_places_origin/lib/main.dart
+++ Academind/great_places_origin/lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './providers/great_places.dart';
+import './screens/add_place_screen.dart';
+import './screens/places_list_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) {},
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
       child: MaterialApp(
-        title: 'Great Places',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.amber,
-        ),
-        home: Scaffold(body: Center(child: Text("starting-setup"))),
-      ),
+          title: 'Great Places',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          }),
     );
   }
 }
