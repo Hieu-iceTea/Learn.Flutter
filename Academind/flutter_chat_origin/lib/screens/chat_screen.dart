264b8ed402f8ed322ecdd589741ea16219d78f9a
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-03 23:44:53 +0700

    UPDATE: flutter_chat_origin

---
+++ Academind/flutter_chat_origin/lib/screens/chat_screen.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/j5z7jsRUaqoY6gdP8G7O/messages')
+            .snapshots(),
+        builder: (ctx, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data!.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/j5z7jsRUaqoY6gdP8G7O/messages')
+              .add({'text': 'This was added by clicking the button!'});
+        },
+      ),
+    );
+  }
+}
