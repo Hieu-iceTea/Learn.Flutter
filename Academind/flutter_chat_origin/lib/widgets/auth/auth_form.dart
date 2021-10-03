2b8a4b884a36b85acfc0c7f01619bb4fd04c91c3
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-04 00:23:31 +0700

    UPDATE: flutter_chat_origin

--- Academind/flutter_chat_origin/lib/widgets/auth/auth_form.dart
+++ Academind/flutter_chat_origin/lib/widgets/auth/auth_form.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
+  AuthForm(this.submitFn);
+
+  final void Function(
+    String email,
+    String password,
+    String userName,
+    bool isLogin,
+  ) submitFn;
+
   @override
   _AuthFormState createState() => _AuthFormState();
 }
@@ -18,10 +27,7 @@ class _AuthFormState extends State<AuthForm> {
 
     if (isValid) {
       _formKey.currentState!.save();
-      print(_userEmail);
-      print(_userName);
-      print(_userPassword);
-      // Use those values to send our auth request ...
+      widget.submitFn(_userEmail, _userPassword, _userName, _isLogin);
     }
   }
 
