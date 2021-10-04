74efa8726ce42d7a0237abfefb2942fc6d17c67a
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-04 11:46:56 +0700

    UPDATE: flutter_chat_origin

--- Academind/flutter_chat_origin/lib/widgets/chat/messages.dart
+++ Academind/flutter_chat_origin/lib/widgets/chat/messages.dart
@@ -1,31 +1,44 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'message_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('chat')
-          .orderBy(
-            'createdAt',
-            descending: true,
-          )
-          .snapshots(),
-      builder: (ctx, AsyncSnapshot<QuerySnapshot> chatSnapshot) {
-        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, AsyncSnapshot<FirebaseUser> futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        final chatDocs = chatSnapshot.data!.documents;
-        return ListView.builder(
-          reverse: true,
-          itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) => Text(
-            chatDocs[index]['text'],
-          ),
-        );
+        return StreamBuilder(
+            stream: Firestore.instance
+                .collection('chat')
+                .orderBy(
+                  'createdAt',
+                  descending: true,
+                )
+                .snapshots(),
+            builder: (ctx, AsyncSnapshot<QuerySnapshot> chatSnapshot) {
+              if (chatSnapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final chatDocs = chatSnapshot.data!.documents;
+              return ListView.builder(
+                reverse: true,
+                itemCount: chatDocs.length,
+                itemBuilder: (ctx, index) => MessageBubble(
+                  chatDocs[index]['text'],
+                  chatDocs[index]['userId'] == futureSnapshot.data!.uid,
+                ),
+              );
+            });
       },
     );
   }
