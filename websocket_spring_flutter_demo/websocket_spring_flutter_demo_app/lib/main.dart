e6ae91ffdcf9113bacb42f4a3f46705af256d9a5
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-28 21:13:01 +0700

    UPDATE: version stomp_dart_client

--- websocket_spring_flutter_demo/websocket_spring_flutter_demo_app/lib/main.dart
+++ websocket_spring_flutter_demo/websocket_spring_flutter_demo_app/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:stomp_dart_client/stomp_frame.dart';
 /// Nguồn tham khảo: https://umes4ever.medium.com/real-time-application-using-websocket-spring-boot-java-react-js-flutter-eb87fe95f94f
 
 // use this if you are using emulator. localhost is mapped to 10.0.2.2 by default.
-final socketUrl = 'http://192.168.1.106:8080/ws-message'; //192.168.1.106
+const socketUrl = 'http://192.168.1.106:8080/ws-message'; //192.168.1.106
 
 void main() {
   runApp(MyApp());
@@ -40,32 +40,44 @@ class _MyHomePageState extends State<MyHomePage> {
 
   String message = '';
 
-  void onConnect(StompClient client, StompFrame frame) {
-    client.subscribe(
-        destination: '/topic/message',
-        callback: (StompFrame frame) {
-          if (frame.body != null) {
-            Map<String, dynamic> result = json.decode(frame.body);
-            //print(result['message']);
-            setState(() => message = result['message']);
-          }
-        });
+  void onConnectCallback(StompFrame connectFrame) {
+    // client is connected and ready
+
+    stompClient!.subscribe(
+      destination: '/topic/message',
+      callback: (StompFrame frame) {
+        if (frame.body != null) {
+          Map<String, dynamic> result = json.decode(frame.body!);
+          setState(() => message = result['message']);
+        }
+      },
+    );
   }
 
   @override
   void initState() {
-    super.initState();
-
     if (stompClient == null) {
       stompClient = StompClient(
-          config: StompConfig.SockJS(
-            url: socketUrl,
-            onConnect: onConnect,
-            onWebSocketError: (dynamic error) => print(error.toString()),
-          ));
+        config: StompConfig.SockJS(
+          url: socketUrl,
+          onConnect: onConnectCallback,
+          onWebSocketError: (dynamic error) => print(error.toString()),
+        ),
+      );
 
       stompClient!.activate();
     }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (stompClient != null) {
+      stompClient!.deactivate();
+    }
+
+    super.dispose();
   }
 
   @override
@@ -78,25 +90,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Your message from server:',
             ),
             Text(
-              '$message',
+              message,
               style: Theme.of(context).textTheme.bodyText1,
             ),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  @override
-  void dispose() {
-    if (stompClient != null) {
-      stompClient!.deactivate();
-    }
-
-    super.dispose();
   }
 }
